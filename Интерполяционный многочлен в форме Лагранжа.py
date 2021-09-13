b67589ff073d821434626ff1104dc1285ffdccfe
danilaLunin13 <78364985+danilaLunin13@users.noreply.github.com>
2021-09-13 23:19:08 +0300

    Update Интерполяционный многочлен в форме Лагранжа.py

--- Интерполяционный многочлен в форме Лагранжа.py
+++ Интерполяционный многочлен в форме Лагранжа.py
@@ -1,5 +1,23 @@
-from sympy.abc import x
-from sympy import expand
+import sympy as sym
+
+class Pol_Lagrange:
+    
+    def __init__(self, X, Y):
+        self.x = sym.Symbol('x')
+        self.ans = 0
+        for i in range(len(X)):
+            M = X[:]
+            x_k = M[i]
+            pol = 1
+            M.remove(x_k)
+            for j in M:
+                pol *= self.x - j
+            for k in M:
+                pol /= x_k - k
+            self.ans += Y[i]*pol
+    
+    def Value(self, arg):
+        return self.ans.subs(self.x, arg)
 
 X = input().split()
 X = list(map(float, X))
@@ -7,18 +25,7 @@ X = list(map(float, X))
 Y = input().split()
 Y = list(map(float, Y))
 
-def aux_pol(X, k):
-    M = X
-    x_k = M[k]
-    pol = 1
-    M.remove(x_k)
-    for i in M:
-        pol *= x - i
-    for i in M:
-        pol /= x_k - i
-    return pol
+x = int(input())
 
-ans = 0
-for i in range(len(X)):
-    ans += Y[i]*aux_pol(X[:],i)
-print(expand(ans))
+Pol = Pol_Lagrange(X, Y)
+print(Pol.Value(x))
